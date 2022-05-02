7928e322102d8c65f09ae9b11df53d8d9794d1d6
Mitchell Rosen <mitchellwrosen@gmail.com>
2022-05-01 21:47:49 -0400

    implement init

--- src/Termbox2.hs
+++ src/Termbox2.hs
@@ -1,3 +1,38 @@
-module Termbox2 () where
+module Termbox2
+  ( init,
+  )
+where
 
--- import qualified Termbox2.Internal as C
+import Control.Exception (Exception, throwIO)
+import qualified Data.ByteString.Unsafe as ByteString
+import Data.Text (Text)
+import qualified Data.Text.Encoding as Text
+import Foreign.C.Types (CInt)
+import qualified Termbox2.Internal as C
+import Prelude hiding (init)
+
+data TermboxException = TermboxException
+  { function :: Text,
+    message :: Text
+  }
+  deriving stock (Show)
+  deriving anyclass (Exception)
+
+init :: IO ()
+init = do
+  result <- C.init
+  if
+      | result == C._OK -> pure ()
+      | result == C._ERR_INIT_ALREADY -> pure ()
+      | otherwise -> exception "init" result
+
+exception :: Text -> CInt -> IO a
+exception function code = do
+  message <- strerror code
+  throwIO TermboxException{function, message}
+
+strerror :: CInt -> IO Text
+strerror n = do
+  cstring <- C.strerror n
+  bytes <- ByteString.unsafePackCString cstring
+  pure (Text.decodeUtf8 bytes)
--- src/Termbox2/Internal.hs
+++ src/Termbox2/Internal.hs
@@ -291,6 +291,9 @@ foreign import ccall unsafe "tb_hide_cursor"
 foreign import ccall unsafe "tb_init"
   init :: IO CInt
 
+foreign import ccall unsafe "tb_last_errno"
+  last_errno :: IO CInt
+
 foreign import ccall safe "tb_peek_event"
   peek_event :: Ptr Event -> CInt -> IO CInt
 
@@ -320,6 +323,9 @@ foreign import ccall unsafe "tb_set_output_mode"
 
 foreign import ccall unsafe "tb_shutdown"
   shutdown :: IO CInt
+
+foreign import ccall unsafe "tb_strerror"
+  strerror :: CInt -> IO (Ptr CChar)
 
 foreign import ccall unsafe "tb_width"
   width :: IO CInt

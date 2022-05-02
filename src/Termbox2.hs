9b2fd7d935c26d0a978d9a72d18bc7de663d0adb
Mitchell Rosen <mitchellwrosen@gmail.com>
2022-05-01 21:50:20 -0400

    implement shutdown

--- src/Termbox2.hs
+++ src/Termbox2.hs
@@ -1,5 +1,6 @@
 module Termbox2
   ( init,
+    shutdown,
   )
 where
 
@@ -11,13 +12,6 @@ import Foreign.C.Types (CInt)
 import qualified Termbox2.Internal as C
 import Prelude hiding (init)
 
-data TermboxException = TermboxException
-  { function :: Text,
-    message :: Text
-  }
-  deriving stock (Show)
-  deriving anyclass (Exception)
-
 init :: IO ()
 init = do
   result <- C.init
@@ -26,13 +20,29 @@ init = do
       | result == C._ERR_INIT_ALREADY -> pure ()
       | otherwise -> exception "init" result
 
-exception :: Text -> CInt -> IO a
-exception function code = do
-  message <- strerror code
-  throwIO TermboxException{function, message}
+shutdown :: IO ()
+shutdown = do
+  result <- C.shutdown
+  if
+      | result == C._OK -> pure ()
+      | otherwise -> exception "shutdown" result
 
 strerror :: CInt -> IO Text
 strerror n = do
   cstring <- C.strerror n
   bytes <- ByteString.unsafePackCString cstring
   pure (Text.decodeUtf8 bytes)
+
+--
+
+data TermboxException = TermboxException
+  { function :: Text,
+    message :: Text
+  }
+  deriving stock (Show)
+  deriving anyclass (Exception)
+
+exception :: Text -> CInt -> IO a
+exception function code = do
+  message <- strerror code
+  throwIO TermboxException {function, message}
